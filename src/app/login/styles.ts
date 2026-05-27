37dec70728e4a28c367f1b5b0fed38eb2d0d0b4f
Philip Wisner <philipwisner@gmail.com>
2026-05-26 22:07:55 -0400

    grouped styles

---
+++ src/app/login/styles.ts
@@ -0,0 +1,33 @@
+import { styled } from "@/styled-system/jsx";
+
+export const PageBackground = styled("div", {
+  base: {
+    transition: "all 0.2s ease",
+    minHeight: "100vh",
+    display: "flex",
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    px: "8",
+    py: "12",
+    bg: "background",
+  },
+});
+
+export const AuthContainerContent = styled("div", {
+  base: {
+    maxWidth: "450px",
+    width: "100%",
+    height: "auto",
+  },
+});
+
+export const FormContainer = styled("form", {
+  base: {
+    width: "100%",
+    marginTop: "8",
+    gap: "5",
+    display: "flex",
+    flexDirection: "column",
+  },
+});

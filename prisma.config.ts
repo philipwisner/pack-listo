31cbcd4f17f1332d605dec1e0463a9d223432cd3
Philip Wisner <philipwisner@gmail.com>
2026-05-29 22:46:00 -0400

    update url

--- prisma.config.ts
+++ prisma.config.ts
@@ -32,7 +32,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
-    shadowDatabaseUrl: process.env["DIRECT_URL"],
+    url: process.env["DIRECT_URL"],
   },
 });

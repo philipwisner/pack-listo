2a82779c7eb1a813a6379cc3b01d856ff4c5bcad
Philip Wisner <philipwisner@gmail.com>
2026-07-06 19:57:27 -0400

    allow scaling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,8 +37,8 @@ const dmSans = DM_Sans({
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
+  // maximumScale: 1,
+  // userScalable: false,
 };
 
 export default function RootLayout({
